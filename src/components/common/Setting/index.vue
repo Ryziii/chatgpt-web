1249d54ceb795e0bc0561855209168a3c6f49ce3
rysiw <az.convict@hotmail.com>
2023-04-28 16:29:41 +0800

    update：删除用户设置页

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -2,8 +2,6 @@
 import { computed, ref } from 'vue'
 import { NModal, NTabPane, NTabs } from 'naive-ui'
 import General from './General.vue'
-import Advanced from './Advanced.vue'
-import About from './About.vue'
 import { SvgIcon } from '@/components/common'
 
 interface Props {
@@ -45,22 +43,6 @@ const show = computed({
           <div class="min-h-[100px]">
             <General />
           </div>
-        </NTabPane>
-        <NTabPane v-if="isChatGPTAPI" name="Advanced" tab="Advanced">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:equalizer-line" />
-            <span class="ml-2">{{ $t('setting.advanced') }}</span>
-          </template>
-          <div class="min-h-[100px]">
-            <Advanced />
-          </div>
-        </NTabPane>
-        <NTabPane name="Config" tab="Config">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <span class="ml-2">{{ $t('setting.config') }}</span>
-          </template>
-          <About />
         </NTabPane>
       </NTabs>
     </div>
